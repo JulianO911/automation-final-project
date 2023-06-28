b6b888b47bbc5882c84f94ee3f1778b067bcd794
Julian Ospina Murgueitio <Julian.Ospina@endava.com>
2023-06-28 18:53:57 -0500

    continuos integration 3

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: "sto3ye",
   e2e: {
+    video: false,
+    screenshotOnRunFailure: false,
     setupNodeEvents(on, config) {
       
     },
