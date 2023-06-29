d6290a174fff4f16bc41b1db32daccc3c514c018
Julian Ospina Murgueitio <Julian.Ospina@endava.com>
2023-06-28 20:54:39 -0500

    mockawesome delete adjustments

--- cypress.config.js
+++ cypress.config.js
@@ -3,17 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: "sto3ye",
   e2e: {
-    video: false,
-    screenshotOnRunFailure: false,
-    reporter: "mochawesome",
-    reporterOptions: {
-      charts: true,
-      html: true,
-      json: true,
-      reportDir: "cypress/reports",
-      reportFilename: "report",
-      overwrite: true
-    },
     setupNodeEvents(on, config) {
       
     },
